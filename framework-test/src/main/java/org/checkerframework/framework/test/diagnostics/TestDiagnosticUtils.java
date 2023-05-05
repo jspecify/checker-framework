2bb7bafe7e3ffaac7f58787ca5b43a5c16265ba9
Chris Povirk <cpovirk@google.com>
2023-05-05 13:55:49 -0400

    Teach the old-style samples test that lines with `test:cannot-convert` indicate expected diagnostics.
    This is necessary before we can merge `main` into
    `samples-google-prototype`.
    
    (#33)

--- framework-test/src/main/java/org/checkerframework/framework/test/diagnostics/TestDiagnosticUtils.java
+++ framework-test/src/main/java/org/checkerframework/framework/test/diagnostics/TestDiagnosticUtils.java
@@ -351,7 +351,8 @@ public class TestDiagnosticUtils {
               true);
       return new TestDiagnosticLine(
           filename, lineNumber, line, Collections.singletonList(diagnostic));
-    } else if (trimmedLine.startsWith("// jspecify_")) {
+    } else if (trimmedLine.startsWith("// jspecify_")
+        || trimmedLine.startsWith("// test:cannot-convert")) {
       TestDiagnostic diagnostic =
           fromJSpecifyFileComment(filename, errorLine, trimmedLine.substring(3));
       return new TestDiagnosticLine(
